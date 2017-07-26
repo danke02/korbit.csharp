504af32438aeb01d901644a13415f87b2d238f01
lisa <lisa@odinsoftware.co.kr>
2017-07-26 14:29:27 +0900

    debug step

---
+++ src/korbit.api.s16/api/trading/tradeApi.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace XCT.BaseLib.API.Korbit.Trading
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class KTradeApi
+    {
+        private string __connect_key;
+        private string __secret_key;
+        private string __user_name;
+        private string __user_password;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public KTradeApi(string connect_key, string secret_key, string user_name, string user_password)
+        {
+            __connect_key = connect_key;
+            __secret_key = secret_key;
+            __user_name = user_name;
+            __user_password = user_password;
+        }
+
+        private KorbitClient __trade_client = null;
+
+        private KorbitClient TradeClient
+        {
+            get
+            {
+                if (__trade_client == null)
+                    __trade_client = new KorbitClient(__connect_key, __secret_key, __user_name, __user_password);
+                return __trade_client;
+            }
+        }
+    }
+}
--- src/korbit.sample.c11/Program.cs
+++ src/korbit.sample.c11/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using XCT.BaseLib.API.Korbit.Public;
+using XCT.BaseLib.API.Korbit.Trading;
+using XCT.BaseLib.API.Korbit.User;
 
 namespace Korbit.Sample
 {
@@ -8,24 +10,57 @@ namespace Korbit.Sample
         /// <summary>
         /// 1. Public API
         /// </summary>
-        public static async void XPublicApi()
+        public static async void XPublicApi(int debug_step = 1)
         {
             var _public_api = new KPublicApi();
 
-            var _ticker = await _public_api.Ticker();
-            Console.WriteLine(_ticker.last);
+            if (debug_step == 1)
+            {
+                var _ticker = await _public_api.Ticker();
+                Console.WriteLine(_ticker.last);
+            }
 
-            var _deailed_ticker = await _public_api.DetailedTicker();
-            Console.WriteLine(_deailed_ticker.last);
+            if (debug_step == 1)
+            {
+                var _deailed_ticker = await _public_api.DetailedTicker();
+                Console.WriteLine(_deailed_ticker.last);
+            }
 
-            var _order_book = await _public_api.OrderBook();
-            Console.WriteLine(_order_book.timestamp);
+            if (debug_step == 1)
+            {
+                var _order_book = await _public_api.OrderBook();
+                Console.WriteLine(_order_book.timestamp);
+            }
 
-            var _transactions = await _public_api.Transactions();
-            Console.WriteLine(_transactions.Count);
+            if (debug_step == 1)
+            {
+                var _transactions = await _public_api.Transactions();
+                Console.WriteLine(_transactions.Count);
+            }
 
-            var _constants = await _public_api.Constants();
-            Console.WriteLine(_constants.minTradableLevel);
+            if (debug_step == 1)
+            {
+                var _constants = await _public_api.Constants();
+                Console.WriteLine(_constants.minTradableLevel);
+            }
+        }
+
+        /// <summary>
+        /// 2. User API
+        /// </summary>
+        public static async void XUserApi(int skip_step = 3)
+        {
+            var __info_api = new KUserApi("", "", "", "");
+
+        }
+
+        /// <summary>
+        /// 3. Trade API
+        /// </summary>
+        public static async void XTradeApi(int skip_step = 1)
+        {
+            var __trade_api = new KTradeApi("", "", "", "");
+
         }
 
         /// <summary>
@@ -39,6 +74,14 @@ namespace Korbit.Sample
             // 1. Public API
             if (_debug_step == 1)
                 XPublicApi();
+
+            // 2. Private API
+            if (_debug_step == 2)
+                XUserApi();
+
+            // 3. Trade API
+            if (_debug_step == 3)
+                XTradeApi();
 
             Console.WriteLine("hit any key to quit...");
             Console.ReadKey();
