3d10b81215f55a51545bb19ec82a7583f5eab0c4
lisa <lisa@odinsoftware.co.kr>
2017-09-18 13:15:59 +0900

    inserted n46 version project for peter

---
+++ src/korbit.api.n46/lib/types/orderCategory.cs
@@ -0,0 +1,23 @@
+﻿namespace XCT.BaseLib.Types
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public enum OrderCategory
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        bid,
+
+        /// <summary>
+        /// 
+        /// </summary>
+        ask,
+
+        /// <summary>
+        /// 
+        /// </summary>
+        all
+    }
+}
---
+++ src/korbit.api.n46/lib/types/timeSymbol.cs
@@ -0,0 +1,23 @@
+﻿namespace XCT.BaseLib.Types
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public enum TimeSymbol
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        minute,
+
+        /// <summary>
+        /// 
+        /// </summary>
+        hour,
+
+        /// <summary>
+        /// 
+        /// </summary>
+        day
+    }
+}
